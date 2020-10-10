f5d1559ce3c741276ffaed1da9c4de3c425eb45a
efratec <brunopaduasc@gmail.com>
2020-10-10 13:54:50 -0300

    commit new changes - Efraim Tenorio

--- api/src/main/java/beertech/becks/api/repositories/TransactionRepository.java
+++ api/src/main/java/beertech/becks/api/repositories/TransactionRepository.java
@@ -4,4 +4,7 @@ import beertech.becks.api.entities.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+
+
 }
---
+++ api/src/main/java/beertech/becks/api/service/TransactionService.java
@@ -0,0 +1,9 @@
+package beertech.becks.api.service;
+
+import beertech.becks.api.entities.Transaction;
+
+public interface TransactionService {
+
+    Transaction createTransaction(Transaction transaction);
+
+}
